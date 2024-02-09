bcdb6e981b78800e5344a8acd78ca945d5879dd6
hosted-fornet <hosted-fornet@protonmail.com>
2024-02-08 18:55:01 -0800

    add basic logic for provider

--- ml/ml/src/lib.rs
+++ ml/ml/src/lib.rs
@@ -29,6 +29,16 @@ fn is_expected_channel_id(
     Ok(channel_id == current_channel_id)
 }
 
+fn get_providers() -> anyhow::Result<Vec<String>> {
+    // TODO: make this real
+    Ok(vec!["fake2.os".into()])
+}
+
+fn ping_provider(provider: &str, our: &Address) -> anyhow::Result<bool> {
+    // TODO: make this real
+    Ok(true)
+}
+
 fn handle_ws_message(
     connection: &mut Option<Connection>,
     message: Message,
@@ -74,29 +84,53 @@ fn handle_ws_message(
 }
 
 fn handle_message(
+    our: &Address,
     connection: &mut Option<Connection>,
 ) -> anyhow::Result<()> {
-    let Ok(message) = await_message() else {
-        return Ok(());
-    };
+    let message = await_message()?;
 
     if serde_json::json!("Run") != serde_json::from_slice::<serde_json::Value>(message.body())? {
         handle_ws_message(connection, message)?;
     } else {
-        let Some(Connection { channel_id }) = connection else {
-            panic!("");
-        };
+        if message.is_request() {
+            if let Some(Connection { channel_id }) = connection {
+                Request::to("our@http_server:distro:sys".parse::<Address>()?)
+                    .body(serde_json::to_vec(&http::HttpServerAction::WebSocketExtPushOutgoing {
+                        channel_id: *channel_id,
+                        message_type: http::WsMessageType::Binary,
+                        desired_reply_type: MessageType::Response,
+                    })?)
+                    .expects_response(15)
+                    .inherit(true)
+                    .send()?;
+                return Ok(());
+            };
+            // Read in blob since pinging providers will clear it (new messages overwrite).
+            let Some(blob) = get_blob() else {
+                // No blob -> no message.
+                return Ok(());
+            };
+            let body = message.body();
 
-        Request::new()
-            .target("our@http_server:distro:sys".parse::<Address>()?)
-            .body(serde_json::to_vec(&http::HttpServerAction::WebSocketExtPushOutgoing {
-                channel_id: *channel_id,
-                message_type: http::WsMessageType::Binary,
-                desired_reply_type: MessageType::Response,
-            })?)
-            .expects_response(15)
-            .inherit(true)
-            .send()?;
+            let providers = get_providers()?;
+            for provider in providers {
+                if ping_provider(&provider, our).is_ok_and(|is_live| is_live) {
+                    let provider_address = Address::new(provider, our.process.clone());
+                    Request::to(provider_address)
+                        .body(body)
+                        .blob(blob)
+                        .expects_response(15)
+                        .send()?;
+
+                    return Ok(());
+                }
+            }
+        } else {
+            Response::new()
+                .body(serde_json::to_vec(&serde_json::json!("Run"))?)
+                .inherit(true)
+                .send()?;
+        }
     }
 
     Ok(())
@@ -111,7 +145,7 @@ fn init(our: Address) {
     http::bind_ext_path("/").unwrap();
 
     loop {
-        match handle_message(&mut connection) {
+        match handle_message(&our, &mut connection) {
             Ok(()) => {}
             Err(e) => {
                 println!("{our}: error: {:?}", e);
